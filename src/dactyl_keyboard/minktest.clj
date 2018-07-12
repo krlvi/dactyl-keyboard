0c34802cdbb33b1f374fef094d7e18e40f68c845
Jared Jennings <jjennings@fastmail.fm>
2018-07-11 23:26:10 -0400

    Add AGPLv3 preambles to pertinent files.
    Since I've now written some parts of this in OpenSCAD, and I haven't
    really split the manually-written OpenSCAD source code from that which
    is generated, the simple statements in the README are no longer
    completely unambiguous. These preambles are. I've applied them with
    https://github.com/jaredjennings/apply-preambles, so they'll all be the
    same, and it should be easy to make your editor fold them up.

--- src/dactyl_keyboard/minktest.clj
+++ src/dactyl_keyboard/minktest.clj
@@ -1,3 +1,20 @@
+;; --- BEGIN AGPLv3-preamble ---
+;; Dactyl Marshmallow ergonomic keyboard generator
+;; Copyright (C) 2015, 2018 Matthew Adereth and Jared Jennings
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; --- END AGPLv3-preamble ---
 (ns dactyl-keyboard.minktest
   (:refer-clojure :exclude [use import])
   (:require [scad-clj.scad :refer :all]
