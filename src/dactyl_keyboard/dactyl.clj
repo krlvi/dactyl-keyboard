c44631a361c0b62f0c061365ef50eb07d8bb5127
Jared Jennings <jjennings@fastmail.fm>
2018-11-25 22:39:19 -0500

    make bottom joints higher-frequency in Z direction.
    There was just one angle across the joint between dm-bot-r00 and dm-bot-r01, so that joint could have slid in the Z direction when pressed together.

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -482,7 +482,7 @@
 (def bottom-egghex-minor-radius 30)
 ;; you may have to change these when you change the minor radius
 (def bottom-lace-holes-per-side 3)
-(def bottom-eggcrate-waves-per-side [0 3 7])
+(def bottom-eggcrate-waves-per-side [0 3 10])
 
 ; set so that there aren't any little bits.
 (defn offset-bottom-slices [shape]
